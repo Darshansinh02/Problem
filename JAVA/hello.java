4ce3bb12b58b85fc296d944568f055528088ec0c
Darshansinh02 <vasadiyadarshansinh@gmail.com>
2026-01-11 18:18:08 +0530

    hello

--- JAVA/hello.java
+++ JAVA/hello.java
@@ -2,5 +2,6 @@ public class hello {
     //this is comment
     public static void main(String[] args) {
         System.out.println("HELLO WORLD");
+        System.out.println("I AM Darshansinh02");
     }
 }
