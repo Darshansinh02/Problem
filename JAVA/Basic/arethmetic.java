24fac43399e1456658054527ccab4033fcce505a
Darshansinh02 <vasadiyadarshansinh@gmail.com>
2026-01-12 12:59:24 +0530

    arithmetic operation

---
+++ JAVA/Basic/arethmetic.java
@@ -0,0 +1,17 @@
+package Basic;
+
+public class arethmetic {
+    public static void main(String[] args) {
+        int total,a = 15, b = 10;
+        int div,mul,sub;
+        total = a+b;
+        div = a/b;
+        mul = a*b;
+        sub = a-b;
+
+        System.out.println("TOTAl IS = " +total);
+        System.out.println("DIVISION IS = " +div);
+        System.out.println("MUL IS = " +mul);
+        System.out.println("SUB IS = " +sub);
+    }
+}
