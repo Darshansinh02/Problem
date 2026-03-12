c89d8dba00fcd06218a04ea47be7db2a618628f0
Darshansinh02 <vasadiyadarshansinh@gmail.com>
2026-03-13 00:00:00 +0530

    square & cube

---
+++ C/squarecube.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a,squ,cube;
+    printf("\nENTER NUMBER : ");
+    scanf("%d",&a);
+    squ = a*a;
+    cube = a*a*a;
+    printf("\nSQUARE IS: %d",squ);
+    printf("\nCUBE IS : %d",cube);
+    return 0;
+}
