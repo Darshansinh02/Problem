092490fb0d681969be8e8d256fba0dee273669fe
Darshansinh02 <vasadiyadarshansinh@gmail.com>
2026-03-15 17:49:05 +0530

    Weeks

---
+++ C/Conditionalstatements/switch1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("\nEnter Number Between 1 to 7: ");
+    scanf("%d",&n);
+    switch(n){
+        case 1:
+        printf("\nMonday");
+        break;
+        case 2:
+        printf("\nTuesday");
+        break;
+        case 3:
+        printf("\nWednesday");
+        break;
+        case 4:
+        printf("\nThursday");
+        break;
+        case 5:
+        printf("\nFriday");
+        break;
+        case 6:
+        printf("\nSaturday");
+        break;
+        case 7:
+        printf("\nSunday");
+        break;
+        default:
+        printf("\nENTER NUMBER IS INVALID");
+        break;
+    }
+    return 0;
+}
